324caa743ed55a0957d00df3b31e978ec8721602
xuantruong9x5@gmail.com <xuantruong9x5@gmail.com>
2025-10-03 10:41:52 +0700

    lc-01 Method 2

--- lc-01.js
+++ lc-01.js
@@ -4,6 +4,8 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// 方法1
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
       for (let j = i + 1; j < nums.length; j++) {
@@ -15,4 +17,20 @@ var twoSum = function(nums, target) {
     }
     return [];
 }; // [0, 1]
+
+
+// 方法2
+var twoSum2 = function(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+      const targetNum = target-nums[i];
+      console.log(map);
+      if(map.has(targetNum)){
+        return [map.get(targetNum), i];
+      }
+      map.set(nums[i], i);
+    }
+}
+
 console.log(twoSum([2,7,11,15], 26));
+console.log(twoSum2([2,7,11,15], 26));
